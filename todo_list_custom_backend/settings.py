d6d14f77bc9df8b70fe084e5375e91f4df8cc867
stevensJourney <51082125+stevensJourney@users.noreply.github.com>
2024-11-01 15:24:35 +0200

    feat: add cors support (#9)

--- todo_list_custom_backend/settings.py
+++ todo_list_custom_backend/settings.py
@@ -32,10 +32,12 @@ ALLOWED_HOSTS = [
     '*'
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
